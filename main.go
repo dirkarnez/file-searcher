c96bd953764744681f277dd0abc06061ef7e3f82
Dirk Arnez <smalldirkalex@gmail.com>
2025-08-31 19:33:53 +0800

    - update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
 var (
@@ -15,7 +16,7 @@ var (
 
 func main() {
 	flag.StringVar(&rootDirectory, "rootDirectory", "", "Root directory to find")
-	flag.StringVar(&fileName, "fileName", "", "File name to find")
+	flag.StringVar(&fileName, "fileName", "", "File name to find, regex pattern supported")
 	flag.Parse()
 
 	if len(fileName) < 1 {
@@ -26,22 +27,33 @@ func main() {
 		log.Fatal("No --rootDirectory is given")
 	}
 
-	foundPath, err := FilePathWalkDir(rootDirectory, fileName)
+	matchedPathArray, err := FilePathWalkDir(rootDirectory, fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(foundPath)
+	
+	for _, matchedPath := range matchedPathArray {
+    	fmt.Println(matchedPath)
+    }
 }
 
-func FilePathWalkDir(root string, lookFor string) (string, error) {
-	var foundPath string
+func stringMatch(patternTolookFor, content) bool {
+	match, _ := regexp.MatchString(patternTolookFor, content)
+	return match
+}
+
+func FilePathWalkDir(root string, lookFor string) ([]string, error) {
+	var matchedPathArray []string
+	
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
-			if lookFor == info.Name() {
-				foundPath = path
+			name := info.Name()
+			if lookFor == name || stringMatch(lookFor, name){
+				matchedPathArray = append(matchedPathArray, path)
+				// DO NOT RETURN
 			}
 		}
 		return nil
 	})
-	return foundPath, err
+	return matchedPathArray, err
 }
